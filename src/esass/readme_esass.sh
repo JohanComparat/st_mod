871b41078b29b47de8fe7f157d2ccbe1c63f414c
Johan Comparat <comparat@mpe.mpg.de>
2025-05-08 16:12:13 +0200

    merging with args

--- src/esass/readme_esass.sh
+++ src/esass/readme_esass.sh
@@ -8,11 +8,20 @@ cd $GIT_STMOD/src/esass/
 python merge_events.py 1 1 # ONGOING
 python merge_events_noCLU.py 1 # ONGOING
 
+GE_e4_merge_AGNseed001_SimBKG
+GE_e4_merge_AGNseed001_SimBKG_CLUseed001
+
 # write eSASS commands
-python eRASSX_write_scripts.py # DONE
+python eRASSX_write_scripts.py GE_e4_merge_AGNseed001_SimBKG #
+python eRASSX_write_scripts.py GE_e4_merge_AGNseed001_SimBKG_CLUseed001 #
 
 # execute eSASS commands on test field:
-cd /home/idies/workspace/erosim/Uchuu/LCerass/121048/sim_evt_e4_merge/eSASS
+cd /home/idies/workspace/erosim/Uchuu/LCerass/121048/GE_e4_merge_AGNseed001_SimBKG/eSASS
+sh 121048_pipeline_img1.sh # DONE
+sh 121048_pipeline_det1.sh
+sh 121048_pipeline_Src1.sh
+
+cd /home/idies/workspace/erosim/Uchuu/LCerass/121048/GE_e4_merge_AGNseed001_SimBKG_CLUseed001eSASS
 sh 121048_pipeline_img1.sh # DONE
 sh 121048_pipeline_det1.sh
 sh 121048_pipeline_Src1.sh
