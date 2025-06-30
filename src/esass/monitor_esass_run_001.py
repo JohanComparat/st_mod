b1599dfe1844fded49970dab84a6d046cf38086b
JohanComparat <comparat@mpe.mpg.de>
2025-06-30 16:15:24 +0200

    summary file run

---
+++ src/esass/monitor_esass_run_001.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import subprocess
+from select import select
+
+# Start two subprocesses
+procs = [
+    subprocess.Popen(["bash", "summary_file_run.sh"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, text=True, close_fds=True),
+]
+
+# Continuously monitor output
+while procs:
+    # Remove processes that have finished
+    for p in procs[:]:
+        if p.poll() is not None:
+            # Drain remaining output
+            for line in p.stdout:
+                print(f"[PID {p.pid}]: {line}", end='')
+            p.stdout.close()
+            procs.remove(p)
+
+    if not procs:
+        break
+
+    # Wait until any process has output
+    rlist, _, _ = select([p.stdout for p in procs], [], [], 0.1)
+    for pipe in rlist:
+        line = pipe.readline()
+        if line:
+            # Identify which process produced this
+            pid = next(p.pid for p in procs if p.stdout is pipe)
+            print(f"[PID {pid}]: {line}", end='')
