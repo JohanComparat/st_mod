4aa5195b58959c15bb88be5a8efce0f2252d0126
JohanComparat <comparat@mpe.mpg.de>
2025-08-09 21:55:29 +0200

    runs 001 and 002

--- src/esass/readme_esass.sh
+++ src/esass/readme_esass.sh
@@ -127,6 +127,26 @@ GE_e4_merge_AGNseed00?_SimBKG_CLUseed00? : AGN + BKG + Cluster
 GE_e4_merge_SimBKG : only BKG
 GE_e4_merge_SimBKG_CLUseed00? : BKG + Cluster
 
+
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed001_SimBKG_CLUseed001 > logs/summary_sky_map_GE_e4_merge_AGNseed001_SimBKG_CLUseed001.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed002_SimBKG_CLUseed002 > logs/summary_sky_map_GE_e4_merge_AGNseed002_SimBKG_CLUseed002.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed003_SimBKG_CLUseed003 > logs/summary_sky_map_GE_e4_merge_AGNseed003_SimBKG_CLUseed003.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed004_SimBKG_CLUseed004 > logs/summary_sky_map_GE_e4_merge_AGNseed004_SimBKG_CLUseed004.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed005_SimBKG_CLUseed005 > logs/summary_sky_map_GE_e4_merge_AGNseed005_SimBKG_CLUseed005.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed006_SimBKG_CLUseed006 > logs/summary_sky_map_GE_e4_merge_AGNseed006_SimBKG_CLUseed006.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed007_SimBKG_CLUseed007 > logs/summary_sky_map_GE_e4_merge_AGNseed007_SimBKG_CLUseed007.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_merge_AGNseed008_SimBKG_CLUseed008 > logs/summary_sky_map_GE_e4_merge_AGNseed008_SimBKG_CLUseed008.log & # TODO
+
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed001_SimBKG_CLUseed001 > logs/summary_sky_map_GE_e4_merge_AGNseed001_SimBKG_CLUseed001.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed002_SimBKG_CLUseed002 > logs/summary_sky_map_GE_e4_merge_AGNseed002_SimBKG_CLUseed002.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed003_SimBKG_CLUseed003 > logs/summary_sky_map_GE_e4_merge_AGNseed003_SimBKG_CLUseed003.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed004_SimBKG_CLUseed004 > logs/summary_sky_map_GE_e4_merge_AGNseed004_SimBKG_CLUseed004.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed005_SimBKG_CLUseed005 > logs/summary_sky_map_GE_e4_merge_AGNseed005_SimBKG_CLUseed005.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed006_SimBKG_CLUseed006 > logs/summary_sky_map_GE_e4_merge_AGNseed006_SimBKG_CLUseed006.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed007_SimBKG_CLUseed007 > logs/summary_sky_map_GE_e4_merge_AGNseed007_SimBKG_CLUseed007.log & # TODO
+nohup python eRASSX_write_scripts.py GE_e5_mergefagn0p15_AGNseed008_SimBKG_CLUseed008 > logs/summary_sky_map_GE_e4_merge_AGNseed008_SimBKG_CLUseed008.log & # TODO
+
+
 ## write eSASS commands in the relevant folders
 #nohup python eRASSX_write_scripts.py GE_e4_merge_SimBKG                       > logs/eRASSX_write_scripts_GE_e4_merge_SimBKG.log                       & # DONE
 #
