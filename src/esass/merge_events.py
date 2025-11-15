158d7ad0599097ef84c94d0625c2310053158b97
Nicola Malavasi <nicolamalavasi87@gmail.com>
2025-11-15 09:13:59 +0100

    Updates path to correct cluster simulated event folder

--- src/esass/merge_events.py
+++ src/esass/merge_events.py
@@ -92,7 +92,7 @@ for sky_tile in sky_map_hdu[(sky_map_hdu['OWNER']==2)|(sky_map_hdu['OWNER']==0)]
 	ids_to_replace = np.arange(len(to_replace))[to_replace]
 	N_ev_OBS = len(ids_to_replace)
 	agn_dir = os.path.join(os.environ['UCHUU'], LC_dir, str_field, "eRASS8_SEED_"+str(agn_seed).zfill(3) +"_events_AGN_2025_04" )
-	cluster_dir = os.path.join(os.environ['UCHUU'], LC_dir, str_field, "Att_eRASS8_sixte_v27_SEED_"+str(clu_seed).zfill(3) +"_events_cluster" )
+	cluster_dir = os.path.join(os.environ['UCHUU'], LC_dir, str_field, "Att_eRASS8_sixte_v27_SEED_"+str(clu_seed).zfill(3) +"_events_cluster_Xgas_bHS0.8" )
 	stars_dir   = os.path.join( os.environ['UCHUU'], LC_dir, str_field, 'stars')#, 'simulated_photons_ccd?.fits' )
 	bg_all = []
 	for el in BG_evt_files:
