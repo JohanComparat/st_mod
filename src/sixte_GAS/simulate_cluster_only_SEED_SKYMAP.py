f0171a324c01e6c9b84916ec7d15ab4d8bbb4559
Nicola Malavasi <nicolamalavasi87@gmail.com>
2026-02-21 00:34:29 +0100

    Improves readability of output, possible bugfix?

--- src/sixte_GAS/simulate_cluster_only_SEED_SKYMAP.py
+++ src/sixte_GAS/simulate_cluster_only_SEED_SKYMAP.py
@@ -5,24 +5,14 @@ Uses sixte version 2.7
 To use sixte version 3.0, this needs a significant upgrade
 
 """
-#import subprocess
+import subprocess as sp
 from multiprocessing import Pool
 from functools import partial
 import os
-#import errno
 import sys, glob
-#import healpy
 import numpy as np
 from astropy.io import fits
-from astropy.table import Table, Column
-
-#pix_ids = np.arange(healpy.nside2npix(8) )
-#ra_cen_s, dec_cen_s = healpy.pix2ang(8, pix_ids, nest=True, lonlat=True)
-sky_map_hdu = Table.read(os.path.join(os.environ['GIT_STMOD_DATA'], 'data/models/eROSITA', 'SKYMAPS.fits'))
-sky_map_hdu['ID'] = np.arange(len(sky_map_hdu))
-#import pymangle
-#eRASS_ply = os.path.join(os.environ['GIT_ERASS_SIM'], 'data', 'eRASS_SKYMAPS.ply' )
-#mng = pymangle.Mangle( eRASS_ply )
+from astropy.table import Table
 
 #One iteration for subprocess
 def one_iter_func(sky_tile, other_elements):
@@ -35,47 +25,37 @@ def one_iter_func(sky_tile, other_elements):
     - other_elements: list containing basename, seed, LC_dir, erass_option, sixte_version
     """
 
-    #Splits other_elements into relevant quantities
-    basename, seed, LC_dir, erass_option, sixte_version, log_fn_for_check = other_elements
-    print(seed, LC_dir, erass_option)#, env, erass_option)
-
-    #Loop as usual
+    #Identify sky tile
     sky_tile_id = str(sky_tile['SRVMAP'])
     str_field = str(sky_tile['SRVMAP']).zfill(6)
+
+    #Splits other_elements into relevant quantities
+    basename, seed, LC_dir, erass_option, sixte_version, log_fn_for_check = other_elements
+    print('\nTile {0} - seed: {1} LC_dir: {2} eRASS option: {2}'.format(str_field, seed, LC_dir, erass_option))
+
+    #Loop as usual
     ra_cen = sky_tile['RA_CEN']
     dec_cen = sky_tile['DE_CEN']
-    #simput_files_all = np.array(glob.glob( os.path.join(os.environ['UCHUU'], LC_dir, str_field, 'Xgas_bHS0.8_simput_N_???.fits')))
-    #simput_files_all.sort()
-    print(sky_tile_id)#, simput_files_all)
-    #for simput_file_i in simput_files_all[:1]:
+    print('\nTile {0} - sky tile ID: {1}'.format(str_field, sky_tile_id))
     #Next line was problematic on 07.11.2025, rseppi added split_simput to handle simput files larger than 1000
     #simput_files = np.array([os.path.join(os.environ['UCHUU'], LC_dir, str_field, basename+'_simput_final.fits'), ])
     simput_file = os.path.join(os.environ['UCHUU'], LC_dir, str_field, basename+'_simput_final.fits')
     simput_files = split_simput(simput_file)
     data_dir = os.path.join(os.environ['UCHUU'], LC_dir, str_field, erass_option + "_sixte_"+ sixte_version+ "_SEED_"+str(seed).zfill(3) +"_events_cluster_"+basename )
-    print('outputs here',data_dir)
-    os.system('mkdir -p '+data_dir )
+    print('\nTile {0} - output path: {1}'.format(str_field, data_dir))
+
+    #Select attitude file and time interval
     if erass_option=='Att_eRASS8':
         p_2_att_file = "/home/idies/workspace/erosim/erosita_attitude/eRASS_4yr_epc85_att.fits"
-        t_starts = np.array([ 617943605 ])
-        t_stops = np.array([ 744174005 ])
-    #ccd1_files = os.path.join( data_dir, 't0erass_ccd?_???.fits' )
-    #os.system('rm ' + ccd1_files)
-    #if os.path.isfile(ccd1_file)==False:
-    #break
-    for jj, (t0, t1) in enumerate( zip( t_starts, t_stops ) ):
-        print('+++++++++++++++++++++++++++++++++++++++++++++++++')
-        print('+++++++++++++++++++++++++++++++++++++++++++++++++')
-        print('STEP', jj, (t0, t1))
-        print('+++++++++++++++++++++++++++++++++++++++++++++++++')
-        print('+++++++++++++++++++++++++++++++++++++++++++++++++')
+        t_starts = np.array([617943605])
+        t_stops = np.array([744174005])
+
+    #Iterate over steps
+    for jj, (t0, t1) in enumerate(zip(t_starts, t_stops)):
+        print('\nTile {0} - Step: {1} t_starts: {2} t_stops: {3}'.format(str_field, jj, t0, t1))
         bkg = 0
         t_start = t0 # 617943605.0
         exposure = t1 - t0 # 31536000 * 4 # = 4 years  # 31536000 = 1year # 15750000 = 1/2 year
-        # Launch...
-        # 3 files
-        #Simulator(bkg, t_start, exposure, seed, simput_file_1, simput_file, simput_file_2).run_all()
-        # 2 files
         try:
             Simulator(
             jj,
@@ -87,9 +67,10 @@ def one_iter_func(sky_tile, other_elements):
             data_dir,
             ra_cen,
             dec_cen,
-            p_2_att_file).run_all()
+            p_2_att_file,
+            str_field).run_all()
         except(FileNotFoundError):
-            print('missing file for field ', str_field)
+            print('\nTile {0} - Step {1} - file is missing'.format(str_field, jj))
 
     #Save the tile_id to the logfile to mark that it is done
     with open(log_fn_for_check, 'a') as lffc:
@@ -103,6 +84,7 @@ def split_simput(input_simput, max_per=1000, flux_min=8e-16):
     Split a SIMPUT file into multiple parts with at most `max_per` sources each.
     Returns a list of output file paths.
     """
+
     clu_simput = Table.read(input_simput)
 
     # Apply flux cut
@@ -182,6 +164,7 @@ def split_simput(input_simput, max_per=1000, flux_min=8e-16):
 
 # =============================================================================
 
+#Simulator class to manage SIXTE
 class Simulator:
     """
     SIXTE simulator for eROSITA observations.
@@ -189,9 +172,7 @@ class Simulator:
     2. Simulate eROSITA observations of simput, using GTI to speed things up.
     """
 
-    def __init__(self, block_id, with_bkg_par, t_start, exposure, seed, simput, data_dir, ra_cen, dec_cen, p_2_att_file):
-        # def __init__(self, with_bkg_par, t_start, exposure, seed, simput,
-        # simput2, simput3):
+    def __init__(self, block_id, with_bkg_par, t_start, exposure, seed, simput, data_dir, ra_cen, dec_cen, p_2_att_file, str_field):
         """
         :param with_bkg_par: Simulate with particle background.
         :param t_start: Start time of simulation. Input units of [s]
@@ -210,6 +191,7 @@ class Simulator:
         self._ra_cen = ra_cen
         self._dec_cen = dec_cen
         self._p_2_att_file = p_2_att_file
+        self._str_field = str_field
 
     def make_event_directory(self):
         """
@@ -218,9 +200,9 @@ class Simulator:
         try:
             os.makedirs(self._data_dir)
         except OSError as e:
-            print('directory already exists')
+            print('\nTile {0} - Directory already exists'.format(self._str_field))
 
-    def compute_gti(self, simput_ero, gti_file ):
+    def compute_gti(self, simput_ero, gti_file):
         """
         Compute the GTI (good time interval) file given the simput file.
         Use this as input to SIXTE call for reducing computational expense.
@@ -239,25 +221,22 @@ class Simulator:
                 ]
 
         command = " ".join(cmd)
-        print('=========================================')
-        print(command)
-        print('=========================================')
+        print('\nTile {0} - Compute GTI with command:\n{1}'.format(self._str_field, command))
         os.system(command)
         hd=fits.open(gti_file)
         number_of_lines = hd[1].header['NAXIS2']
-        print('file N lines', number_of_lines)
+        print('\nTile {0} - GTI file has {1} lines'.format(self._str_field, number_of_lines))
         if number_of_lines==0:
             cmd_del = 'rm '+gti_file
-            print('=========================================')
-            print(cmd_del)
-            print('=========================================')
+            print('\nTile {0} - Removing GTI file with command:\n{1}'.format(self._str_field, cmd_del))
             os.system(cmd_del)
 
     def run_sixte(self):
         """
         Launch erosim from python.
         """
-        print('N simput files', self._N_simputs)
+
+        print('\nTile {0} - Number of simput files: {1}'.format(self._str_field, self._N_simputs))
         prefix = os.path.join(self._data_dir, self._block_id+"erass_" )
         if self._N_simputs==1:
             cmd = ["erosim",
@@ -275,32 +254,13 @@ class Simulator:
                     "clobber=yes",
                     "chatter=3"
                     ]
-            #cmd = ["runsixt",
-                   #"XMLFile=erosita.xml",
-                    #"Attitude=%s" % self._p_2_att_file,
-                    #"Exposure=%s" % self._exposure,
-                    #"Simput=%s" % self._simput[0],
-                    #"EvtFile=events.fits",
-                    ####"Prefix=%s" % prefix,
-                    ####"RA=%s" % self._ra_cen,
-                    ####"Dec=%s" % self._dec_cen,
-                    ####"GTIFile=%s/erass.gti" % self._data_dir,
-                    ####"TSTART=%s" % self._t_start,
-                    ####"MJDREF=51543.875",
-                    ####"dt=0.5",
-                    ####"Seed=%s" % self._seed,
-                    ####"clobber=yes",
-                    ####"chatter=3"
-                    #]
             if self._with_bkg_par:
                 cmd.append("Background=yes")
             else:
                 cmd.append("Background=no")
 
             command = " ".join(cmd)
-            print('=========================================')
-            print(command)
-            print('=========================================')
+            print('\nTile {0} - Running SIXTE with command:\n{1}'.format(self._str_field, command))
             os.system(command)
             return  # we're done
 
@@ -329,9 +289,7 @@ class Simulator:
             else:
                 cmd.append("Background=no")
             command = " ".join(cmd)
-            print('=========================================')
-            print(command)
-            print('=========================================')
+            print('\nTile {0} - Running SIXTE with command:\n{1}'.format(self._str_field, command))
             os.system(command)
 
         # ---------- >6 SIMPUTS: two runs ----------
@@ -364,10 +322,7 @@ class Simulator:
                 cmd.append("Background=no")
 
             command = " ".join(cmd)
-            print('=========================================')
-            print('FIRST erosim run (SIMPUT 1–6)')
-            print(command)
-            print('=========================================')
+            print('\nTile {0} - FIRST SIXTE RUN (SIMPUT 1–6) - Running SIXTE with command:\n{1}'.format(self._str_field, command))
             os.system(command)
 
             # ---- Second run: remaining SIMPUTs (6,7,...) ----
@@ -402,68 +357,62 @@ class Simulator:
                 cmd1.append("Background=no")
 
             command1 = " ".join(cmd1)
-            print('=========================================')
-            print('SECOND erosim run (SIMPUT 7+ )')
-            print(command1)
-            print('=========================================')
+            print('\nTile {0} - SECOND SIXTE RUN (SIMPUT 7+) - Running SIXTE with command:\n{1}'.format(self._str_field, command1))
             os.system(command1)
 
+    #This is what is called
     def run_all(self):
         """
         Run SIXTE simulation of eRASS 8
         """
-        print('make event directory')
+
+        #Create directory for output
+        print('\nTile {0} - Make event directory'.format(self._str_field))
         self.make_event_directory()
-        print('compute gti with ero_vis')
+        
+        #Compute GTI file
+        print('\nTile {0} - Compute gti with ero_vis'.format(self._str_field))
         if self._N_simputs==1:
             path_to_gti = os.path.join(self._data_dir, "erass.gti")
             self.compute_gti(self._simput[0], path_to_gti)
 
         if self._N_simputs>=2:
             for el in self._simput:
-                gti_file = os.path.join(self._data_dir, "erass_" + os.path.basename(el)[:-4] + ".gti")
+                gti_file = os.path.join(self._data_dir, "erass_" + os.path.basename(el)[:-5] + ".gti")
                 self.compute_gti(el, gti_file)
-            print('merges gti files')
+            print('\nTile {0} - Merging gti files'.format(self._str_field))
             path_to_gti_list = os.path.join(self._data_dir, "gti.list")
-            #print(path_to_gti_list)
             path_to_gti = os.path.join(self._data_dir, "erass.gti")
-            #print(path_to_gti)
             gti_files = glob.glob(self._data_dir + "/*.gti")
             if len(gti_files)>0:
                 command_list = "ls " + self._data_dir + "/*.gti > " + path_to_gti_list
-                print('=========================================')
-                print(command_list)
-                print('=========================================')
+                print('\nTile {0} - Listing GTI files:\n{1}'.format(self._str_field, command_list))
                 os.system(command_list)
-                # ls /data40s/erosim/eRASS/sixte/000/erass_SIMPUT_000000*.gti >
-                # gti.list
                 command_merge = "mgtime ingtis=@" + path_to_gti_list + " outgti=" + path_to_gti + " merge=OR"
-                print('=========================================')
-                print(command_merge)
-                print('=========================================')
+                print('\nTile {0} - Merging GTI files with command:\n{1}'.format(self._str_field, command_merge))
                 os.system(command_merge)
 
         if self._N_simputs==0:
-            print('NO SIMPUT')
+            print('\nTile {0} - No simput'.format(self._str_field))
             return 0.
 
-        print('is there a gti file ?', os.path.isfile(path_to_gti))
+        #If a GTI file is present then run SIXTE        
+        print('\nTile {0} - Using GTI file:\n{1}'.format(self._str_field, os.path.isfile(path_to_gti)))
         if os.path.isfile(path_to_gti):
-            print('run sixte with erosim')
+            print('\nTile {0} - Running SIXTE with erosim'.format(self._str_field))
             self.run_sixte()
-            # delete gti files
-            # elements of
-            list_2_delete = np.array(glob.glob(os.path.join(self._data_dir,  "*.gti" )))
-            print('============================================')
-            print('============================================')
-            print('deleting', list_2_delete)
-            print('============================================')
-            print('============================================')
-            del_list = np.array([ os.remove(el) for el in  list_2_delete])
 
-#erosim Simput=/home/idies/workspace/erosim/Uchuu/LCerass/164087/Xgas_bHS0.8_simput_N_000.fits Prefix=/home/idies/workspace/erosim/Uchuu/LCerass/164087/eRASS8_SEED_001_events_cluster_2025_04/t0erass_ Attitude=/home/idies/workspace/erosim/erosita_attitude/eRASS_4yr_epc85_att.fits RA=163.5 Dec=3.000512456781548 GTIFile=/home/idies/workspace/erosim/Uchuu/LCerass/164087/eRASS8_SEED_001_events_cluster_2025_04/erass.gti TSTART=617943605.0 Exposure=126230400.0 MJDREF=51543.875 dt=0.5 Seed=1 clobber=yes chatter=3 Background=no
+            list_2_delete = np.array(glob.glob(os.path.join(self._data_dir,  "*.gti" )))
+            print('\nTile {0} - Deleting files:\n{1}'.format(self._str_field, list_2_delete))
+            del_list = np.array([os.remove(el) for el in  list_2_delete])
 
 if __name__ == '__main__':
+
+    #Read skymap
+    sky_map_hdu = Table.read(os.path.join(os.environ['GIT_STMOD_DATA'], 'data/models/eROSITA', 'SKYMAPS.fits'))
+    sky_map_hdu['ID'] = np.arange(len(sky_map_hdu))
+
+    #Load basename and seed
     basename = sys.argv[1]
     seed = int(sys.argv[2])
 
@@ -482,7 +431,7 @@ if __name__ == '__main__':
     ncores = int(sys.argv[3])
     joblabel = int(sys.argv[4])
 
-    #Log file - stores tile IDs of tiles already processed. INFORMATION IS ONLY APPENDED
+    #Log file - stores tile IDs of tiles already processed.
     log_fn_for_check = sys.argv[5]
 
     #Open logfile and read what's on it
