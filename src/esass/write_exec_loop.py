bcfa3a2ee1dd1fee2c6c188b72424dfd348fe99d
Nicola Malavasi <nicolamalavasi87@gmail.com>
2026-09-11 01:02:10 +0200

    write exec new seeds

--- src/esass/write_exec_loop.py
+++ src/esass/write_exec_loop.py
@@ -36,8 +36,8 @@ N_per_batch = int(sys.argv[1])
 writetype = sys.argv[2] #total or batch
 
 #Cluster seed list
-cluseed_e4 = [34]
-cluseed_e5 = [0]
+cluseed_e4 = [32, 33, 41]
+cluseed_e5 = [23, 38]
 agnseed_e4 = []
 for cs in cluseed_e4:
     tas = cs%9
